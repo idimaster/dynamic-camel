babbeeb51095cf1bfd34d126f85d15870768e9f1
Dmitry Izumskiy <idimaster@gmail.com>
2019-03-11 22:47:44 -0700

    Add Camel REST endpoints

--- src/main/java/init/EvaluationCamelRouter.java
+++ src/main/java/init/EvaluationCamelRouter.java
@@ -1,7 +1,9 @@
 package init;
 
+import data.EvaluationRequest;
 import data.EvaluationResponse;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -12,6 +14,13 @@ public class EvaluationCamelRouter extends RouteBuilder {
 
     @Override
     public void configure() {
+        restConfiguration()
+                .bindingMode(RestBindingMode.json)
+                .dataFormatProperty("prettyPrint", "true")
+                .apiContextPath("/api-doc")
+                .apiProperty("api.title", "Evaluation API").apiProperty("api.version", "1.0")
+                .apiProperty("cors", "true");
+
         onException(Exception.class)
                 .handled(true)
                 .setBody().constant(new EvaluationResponse(UUID.randomUUID(), "42"));
@@ -23,5 +32,16 @@ public class EvaluationCamelRouter extends RouteBuilder {
         from("direct:test1")
                 .setBody().constant(new EvaluationResponse(UUID.randomUUID(), "44"))
                 .end();
+
+        from("direct:test2")
+                .setBody().constant(new EvaluationResponse(UUID.randomUUID(), "45"))
+                .end();
+
+        rest("v2")
+                .get("/{id}").description("Find by id").outType(EvaluationResponse.class)
+                .to("direct:test1")
+
+                .post("/evaluate").description("Evaluate").type(EvaluationRequest.class).outType(EvaluationResponse.class)
+                .to(ROUTE_NAME);
     }
 }
